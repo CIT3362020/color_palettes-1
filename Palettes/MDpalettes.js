cae8d695e944d8de92b14d8942d7a32efc26f575
Matt Dilena <47456437+FlushedFire@users.noreply.github.com>
2019-02-21 21:58:03 -0500

    Add files via upload

---
+++ Palettes/MDpalettes.js
@@ -0,0 +1,4 @@
+var Moody_Blues = ["#572c6b", "#88579c", "#ca96eb", "#c5c0f0"];
+var Gold_Experience = ["#EBDC94", "#E0BB5C", "#EBE5b9", "#EDB9ED"];
+var Crazy_Diamond = ["#c7e4eb", "#E0abb4", "#c8e8e5", "#e0929b"];
+var Star_Platinum = ["#9088d1", "#Aeabdb", "#e3d78a", "#F4642B"];
