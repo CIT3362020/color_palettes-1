d743a2cb0a8f3f5c8ee3f36f8a6e1f26ef9505f7
jeff <jeff@jeffs-MBP.fios-router.home>
2019-02-21 20:22:12 -0500

    jeffreycolor

--- Palettes/palettes.js
+++ Palettes/palettes.js
@@ -1,2 +1,2 @@
-var blue_monochrome = ["#333676", "#7F81B1", "#545894", "#080B3B"];
-var green_monochrome = ["#27BB8B", "#299672", "#225544", "#18332A"];
+var blue_monochrome = ["#B477AA", "#964A8A", "#78286B", "#3C0033"];
+var green_monochrome = ["#69959D", "#299672", "#225544", "#18332A"];
