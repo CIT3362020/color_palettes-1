5dbe02162e13e424ad09d868f13857e224b5b435
Anthony <ab58680p@pace.edu>
2019-02-22 20:59:17 -0500

    Add anthonypalette

---
+++ Palettes/anthonypalettes.js
@@ -0,0 +1,4 @@
+var blue_monochrome = ["#897AC8", "6B59B6", "442DA4", "311992"];
+var green_monochrome = ["7ADB7A", "57D057", "3CC43C", "0AAA0A"];
+var yellow_monochrome = ["FFE98D", "F5D44B", "EDC729", "D4AE0C"];
+var red_monochrome = ["FF8D8D", "F54B4B", "ED2929", "D40C0C"];
