2c578c24e4947f1e1a5076007791037f38341546
shemberger <sh59972p@pace.edu>
2019-02-27 15:26:24 -0500

    Add steve_palette

--- Palettes/palettes.js
+++ Palettes/palettes.js
@@ -1,2 +1,3 @@
 var blue_monochrome = ["#B477AA", "#964A8A", "#78286B", "#3C0033"];
 var green_monochrome = ["#69959D", "#299672", "#225544", "#18332A"];
+var steve_palette = ["#882D61", "#2E4272", "#413075", "#AAAA39"]
