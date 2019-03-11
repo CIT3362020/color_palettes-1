04fc95f25163991655b71476b6677b22dc5ab6ca
Drew Ku <andrewku123@gmail.com>
2019-03-11 16:24:46 -0400

    Add Andrew's Pallete

--- Palettes/palettes.js
+++ Palettes/palettes.js
@@ -1,2 +1,3 @@
 var blue_monochrome = ["#B477AA", "#964A8A", "#78286B", "#3C0033"];
 var green_monochrome = ["#69959D", "#299672", "#225544", "#18332A"];
+var andrew-s-magenta = ["CB87AF", "A95486", "872D62", "651142"];
