2e24a99fc79557386c4b7da1b8c247cd28f49f5a
Gabriel  Sanxhaku <gs23433n@pace.edu>
2019-03-25 16:25:40 -0400

    add tropical color_palette

--- Palettes/palettes.js
+++ Palettes/palettes.js
@@ -1,2 +1,3 @@
 var blue_monochrome = ["#B477AA", "#964A8A", "#78286B", "#3C0033"];
 var green_monochrome = ["#69959D", "#299672", "#225544", "#18332A"];
+var tropical = ["#10dc9a", "#50dc9b", "#70dc9c", "#80dc9d", "#80dc8e"];
