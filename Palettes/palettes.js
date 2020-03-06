602a6c2a19e1273d58886b32abd60288bc34be3d
ethanlocker <60658281+ethanlocker@users.noreply.github.com>
2020-03-06 15:10:29 -0500

    Add ethan_palette

--- Palettes/palettes.js
+++ Palettes/palettes.js
@@ -1,2 +1,3 @@
 var blue_monochrome = ["#B477AA", "#964A8A", "#78286B", "#3C0033"];
 var green_monochrome = ["#69959D", "#299672", "#225544", "#18332A"];
+var ethan_palette = ['#c2c1c2', '#42213d', '#683257', '#bd4089', '#c91687']
